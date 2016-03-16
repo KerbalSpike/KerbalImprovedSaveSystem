7311f86d05fee106f6fbc2fa5c0e32d1a31a78c9
KerbalSpike <zet.mbox@gmail.com>
2016-03-16 23:18:58 +0100

    Basic SaveAs dialog implemented.

--- KerbalImprovedSaveSystem.cs
+++ KerbalImprovedSaveSystem.cs
@@ -1,21 +1,29 @@
 ﻿using KerbalImprovedSaveSystem.Extensions;
 using KSP.IO;
 using UnityEngine;
+using System;
 
 namespace KerbalImprovedSaveSystem
 {
 	/// <summary>
-	/// Start Kerbal improved savegame system as soon as possible and keep loaded.
+	/// Start Kerbal Improved Save System as soon as possible and keep loaded.
 	/// </summary>
 	[KSPAddon(KSPAddon.Startup.EveryScene, false)] 
-	public class KerbalImprovedSavegameSystem : MonoBehaviour
+	public class KerbalImprovedSaveSystem : MonoBehaviour
 	{
 		// used to identify log entries of this mod
-		public static string modLogTag = "[KISS] ";
+		public static string modLogTag = "[KISS]: ";
 
+		// stuff to configure the GUI
 		private static Rect _windowPosition = new Rect();
-		private GUIStyle _windowStyle, _labelStyle;
+		private static bool _isVisible = false;
+		private GUIStyle _windowStyle, _labelStyle, _buttonStyle, _txtFieldStyle;
 		private bool _hasInitStyles = false;
+
+		// suggested default filename for savegame
+		private string defaultSaveFileName = "";
+		//private string selectedSaveFileName = "";
+
 
 		/// <summary>
 		/// Main part of this Add-on; used to react to keyboard input and do all the magic stuff :)
@@ -23,35 +31,39 @@ namespace KerbalImprovedSaveSystem
 		void Update()
 		{
 			//on Alt-F5
-			if (GameSettings.QUICKSAVE.GetKey() && GameSettings.MODIFIER_KEY.GetKey()) {
-				
-				Debug.Log(modLogTag + "Init GUI.");
+			if (GameSettings.QUICKSAVE.GetKey() && GameSettings.MODIFIER_KEY.GetKey())
+			{
 				if (!_hasInitStyles)
+				{
+					Debug.Log(modLogTag + "Init GUI.");
 					InitStyles();
-				RenderingManager.AddToPostDrawQueue(0, OnDraw);
+				}
 
-				// available SaveModes are: OVERWRITE, APPEND, ABORT
-				SaveMode s = SaveMode.OVERWRITE;
-				string filename = GamePersistence.SaveGame ("MyOwnSaveFile", HighLogic.SaveFolder, s);
-				Debug.Log (modLogTag + "Game saved in '" + filename + "'");
+				if (!_isVisible)
+				{
+					_isVisible = true;
+					defaultSaveFileName = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+					RenderingManager.AddToPostDrawQueue(0, OnDraw);
+				}
 			}
 		}
 
-//		public override void OnSave(ConfigNode node)
+//		void Start()
 //		{
-//			PluginConfiguration config = PluginConfiguration.CreateForType<KerbalImprovedSavegameSystem>();
+//			PluginConfiguration config = PluginConfiguration.CreateForType<KerbalImprovedSaveSystem>();
 //
 //			config.SetValue("Window Position", _windowPosition);
 //			config.save();
 //		}
 //
-//		public override void OnLoad(ConfigNode node)
+//		void OnDestroy()
 //		{
-//			PluginConfiguration config = PluginConfiguration.CreateForType<KerbalImprovedSavegameSystem>();
+//			PluginConfiguration config = PluginConfiguration.CreateForType<KerbalImprovedSaveSystem>();
 //
 //			config.load();
 //			_windowPosition = config.GetValue<Rect>("Window Position");
 //		}
+
 
 		private void OnDraw()
 		{
@@ -59,29 +71,76 @@ namespace KerbalImprovedSaveSystem
 
 			if (_windowPosition.x == 0f && _windowPosition.y == 0f)
 			{
-				_windowPosition = _windowPosition.CenterScreen();
+				PluginConfiguration config = PluginConfiguration.CreateForType<KerbalImprovedSaveSystem>();
+				config.load();
+				_windowPosition = config.GetValue<Rect>("Window Position", _windowPosition.CenterScreen());
 			}
-
 		}
+
 
 		private void OnWindow(int windowId)
 		{
-			GUILayout.BeginHorizontal();
+
+			GUILayout.BeginVertical();
+			GUILayout.Label("Previous saves:", _labelStyle);
+
 			GUILayout.Label("Filename:", _labelStyle);
+			string selectedSaveFileName = GUILayout.TextField(defaultSaveFileName, _txtFieldStyle);
+
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace(); // moves the following buttons to the right
+			if (GUILayout.Button("Save", _buttonStyle))
+			{
+				Save(selectedSaveFileName);
+				Close("SaveDialog completed.");
+			}
+			if (GUILayout.Button("Cancel", _buttonStyle))
+			{
+				Close("SaveDialog aborted by user.");
+			}
 			GUILayout.EndHorizontal();
+			GUILayout.EndVertical();
 
 			GUI.DragWindow();
 		}
 
+
 		private void InitStyles()
 		{
 			_windowStyle = new GUIStyle(HighLogic.Skin.window);
-			_windowStyle.fixedWidth = 250f;
+			_windowStyle.fixedWidth = 300f;
 
 			_labelStyle = new GUIStyle(HighLogic.Skin.label);
 			_labelStyle.stretchWidth = true;
 
+			_buttonStyle = new GUIStyle(HighLogic.Skin.button);
+
+			_txtFieldStyle = new GUIStyle(HighLogic.Skin.textField);
+			_txtFieldStyle.stretchWidth = true;
+
 			_hasInitStyles = true;
+		}
+
+
+		private void Save(string selectedSaveFileName)
+		{
+			//code to save game
+			SaveMode s = SaveMode.OVERWRITE; // available SaveModes are: OVERWRITE, APPEND, ABORT
+			string filename = GamePersistence.SaveGame (selectedSaveFileName, HighLogic.SaveFolder, s);
+			Debug.Log (modLogTag + "Game saved in '" + filename + "'");
+		}
+
+
+		private void Close(string reason)
+		{
+			PluginConfiguration config = PluginConfiguration.CreateForType<KerbalImprovedSaveSystem>();
+			config.SetValue("Window Position", _windowPosition);
+			config.save();
+
+			// code to remove window
+			_isVisible = false;
+			RenderingManager.RemoveFromPostDrawQueue(0, OnDraw);
+			Debug.Log (modLogTag + reason);				
 		}
 	}
 }
